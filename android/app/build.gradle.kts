a1a112e448d19e71a1347dd575a881532888e7c8
ysntopbas <ysntpbss@gmail.com>
2025-11-13 12:26:27 +0300

    feat: Integrate flutter_local_notifications package and update permissions in AndroidManifest.xml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,9 +9,13 @@ android {
     namespace = "com.atqs.prayer_time"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        
+        isCoreLibraryDesugaringEnabled = true 
     }
 
     kotlinOptions {
@@ -40,4 +44,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import app_settings
+import flutter_local_notifications
 import flutter_localization
 import geolocator_apple
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
